455b5678a17c7d238cc290b10a6d830dbc7ef698
Carlos Cerdá Morales <carcermor@alum.us.es>
2025-02-27 21:49:26 +0100

    feat: :sparkles: D02-R4 Booking data model mandatory student 2

---
+++ src/main/java/acme/entities/Booking.java
@@ -0,0 +1,58 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Booking extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -----------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,8}$")
+	@Column(unique = true)
+	private String				locatorCode;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				purchaseMoment;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private TravelClass			travelClass;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	@Automapped
+	private Integer				price;
+
+	@Optional
+	@ValidString(pattern = "^\\d{4}$")
+	@Automapped
+	private String				lastNibble;
+
+}
