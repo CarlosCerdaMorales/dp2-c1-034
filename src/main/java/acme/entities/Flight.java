8650f65e905b00e00bdf9da3db23a419efb04fe6
SergioCantilloBlanco <14sergiocb@gmail.com>
2025-02-27 20:29:47 +0100

    feat: :sparkles: Flight class java model

---
+++ src/main/java/acme/entities/Flight.java
@@ -0,0 +1,53 @@
+
+package acme.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Flight extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -----------------------------------------------
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				flightTag;
+
+	@Mandatory
+	@Automapped
+	private boolean				isSelfTransfer;
+
+	@Mandatory
+	@Automapped
+	@ValidNumber(min = 0)
+	private double				flightCost;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				flightDescription;
+
+	//Relationships-----------------------------------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Leg					leg;
+
+}
