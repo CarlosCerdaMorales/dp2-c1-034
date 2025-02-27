558c19a0b614bfef3954c2e4ec9bc643cbd6dd5a
SergioCantilloBlanco <14sergiocb@gmail.com>
2025-02-27 19:10:21 +0100

    feat: :sparkles: manager role fix to match rest of models

--- src/main/java/acme/roles/Manager.java
+++ src/main/java/acme/roles/Manager.java
@@ -3,22 +3,20 @@ package acme.roles;
 
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
 
 import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidUrl;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -34,25 +32,22 @@ public class Manager extends AbstractRole {
 	//ATRIBUTES-------------------------------------------------------------------------------------
 
 	@Mandatory
-	@NotBlank
+	@Automapped
 	@Pattern(regexp = "^[A-Z]{2,3}\\d{6}$")
-	@Column(name = "manager_code", unique = true)
 	private String				managerCode;
 
 	@Mandatory
-	@NotNull
-	@Min(0)
-	@Column(name = "years_of_experience")
-	private int					yearsOfExperience;
+	@Automapped
+	@ValidScore
+	private int					yearsExperience;
 
 	@Mandatory
 	@ValidMoment
 	@Temporal(TemporalType.TIMESTAMP)
-	@Column()
 	private Date				birthDay;
 
 	@Optional
-	@URL
+	@ValidUrl
 	@Length(max = 255)
 	private String				pictureLink;
 
