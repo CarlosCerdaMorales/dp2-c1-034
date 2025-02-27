05d6bee77c51d82c6595fcf44c4afe01cf8656b5
SergioCantilloBlanco <14sergiocb@gmail.com>
2025-02-27 20:30:22 +0100

    feat: :sparkles: Leg java class model

---
+++ src/main/java/acme/entities/Leg.java
@@ -0,0 +1,62 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Leg extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -----------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2}\\d{4}$")
+	@Automapped
+	private String				flightNumber;
+
+	@Mandatory
+	@Automapped
+	@ValidMoment
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				scheduledDeparture;
+
+	@Mandatory
+	@Automapped
+	@ValidMoment
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				scheduledArrival;
+
+	@Mandatory
+	@Automapped
+	private double				durationInHours;
+
+	@Mandatory
+	@Automapped
+	private FlightStatus		flightStatus;
+
+	//Relationships-----------------------------------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Flight				flight;
+}
