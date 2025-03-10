8aaeda282bc9240e0734737b519220b84e7b4cfd
ClaudiaOviedoGovantes <claudiaogvizcaino@gmail.com>
2025-03-10 21:41:55 +0100

    feat: :sparkles: Airline csv

--- src/main/java/acme/entities/airline/Airline.java
+++ src/main/java/acme/entities/airline/Airline.java
@@ -16,6 +16,7 @@ import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
+import acme.datatypes.Phone;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -38,12 +39,12 @@ public class Airline extends AbstractEntity {
 	@Mandatory
 	@ValidString(pattern = "^[A-Z]{2}X$", max = 3)
 	@Automapped
-	private String				IATACode;
+	private String				iata;
 
 	@Mandatory
 	@ValidUrl
 	@Automapped
-	private String				webSite;
+	private String				website;
 
 	@Mandatory
 	@Valid
@@ -61,8 +62,8 @@ public class Airline extends AbstractEntity {
 	private String				email;
 
 	@Optional
-	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Valid
 	@Automapped
-	private String				phoneNumber;
+	private Phone				phoneNumber;
 
 }
