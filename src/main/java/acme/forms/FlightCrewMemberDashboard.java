ef8f3ef3241111cb541aa42e6fcce4bbe8736938
Loubna Founoun elAoud <loufouela@alum.us.es>
2025-03-12 00:24:34 +0100

    feat: :sparkles: Added FlightCrewMember Dashboard Student 3

---
+++ src/main/java/acme/forms/FlightCrewMemberDashboard.java
@@ -0,0 +1,42 @@
+
+package acme.forms;
+
+import java.util.HashMap;
+import java.util.List;
+
+import acme.client.components.basis.AbstractForm;
+
+public class FlightCrewMemberDashboard extends AbstractForm {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	// The last five destinations they have been assigned. 
+	List<String>				lastFiveDestinationsAssigned;
+
+	// TThe number of legs that have an activity log record with an incident severity ranging from 0 up to 3, 4 up to 7, and 8 up to 10.
+	Integer						numberOfLegsWithActivityLogsLowSeverityLevel;
+	Integer						numberOfLegsWithActivityLogsMediumSeverityLevel;
+	Integer						numberOfLegsWithActivityLogsHighSeverityLevel;
+
+	// The crew members who were assigned with him or her in their last leg. 
+	Integer						crewMembersAssignedInLastLeg;
+
+	// Their flight assignments grouped by their statuses. 
+	HashMap<String, Integer>	numberOfFlightAssignmentsGroupedByTheirStatus;
+
+	// The average, minimum, maximum, and standard deviation of the number of flight assignments they had in the last month.. 
+
+	Double						averageFlightAssignmentsInLastMonth;
+	Double						minFlightAssignmentsInLastMonth;
+	Double						maxFlightAssignmentsInLastMonth;
+	Double						standardDeviationFlightAssignmentsInLastMonth;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
