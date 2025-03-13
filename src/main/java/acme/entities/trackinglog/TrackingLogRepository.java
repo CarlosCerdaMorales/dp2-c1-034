8d59f127e137b73c22945308eff6de5e0052d466
JoseLu2121 <josmorver@alum.us.es>
2025-03-13 13:22:52 +0100

    feat: :coffin: Tracking log attributes fixed and resolutionPercentage validator done

--- src/main/java/acme/entities/trackinglog/TrackingLogRepository.java
+++ src/main/java/acme/entities/trackinglog/TrackingLogRepository.java
@@ -1,6 +1,9 @@
 
 package acme.entities.trackinglog;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.client.repositories.AbstractRepository;
@@ -8,4 +11,6 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface TrackingLogRepository extends AbstractRepository {
 
+	@Query("SELECT t FROM TrackingLog t WHERE t.claim.id = :claimId ORDER BY t.lastUpdateMoment DESC")
+	List<TrackingLog> findLatestTrackingLogByClaim(Integer claimId);
 }
