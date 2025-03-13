bc9aae24f2512919e54142cda3e5586e45a09975
Loubna Founoun elAoud <loufouela@alum.us.es>
2025-03-13 20:31:51 +0100

    fix: :bug: Fixing type on FlightcrewMember Dashboard

--- src/main/java/acme/forms/FlightCrewMemberDashboard.java
+++ src/main/java/acme/forms/FlightCrewMemberDashboard.java
@@ -1,7 +1,7 @@
 
 package acme.forms;
 
-import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 import acme.client.components.basis.AbstractForm;
@@ -11,27 +11,29 @@ public class FlightCrewMemberDashboard extends AbstractForm {
 
 	// Serialisation version --------------------------------------------------
 
-	private static final long	serialVersionUID						= 1L;
+	private static final long serialVersionUID = 1L;
 
 	// Attributes -------------------------------------------------------------
 
-	// The last five destinations they have been assigned. 
-	List<String>				lastFiveDestinationsAssigned;
+	// The last five destinations they have been assigned.
+	List<String> lastFiveDestinationsAssigned;
 
-	// TThe number of legs that have an activity log record with an incident severity ranging from 0 up to 3, 4 up to 7, and 8 up to 10.
-	Integer						numberOfLegsWithActivityLogsLowSeverityLevel;
-	Integer						numberOfLegsWithActivityLogsMediumSeverityLevel;
-	Integer						numberOfLegsWithActivityLogsHighSeverityLevel;
+	// TThe number of legs that have an activity log record with an incident
+	// severity ranging from 0 up to 3, 4 up to 7, and 8 up to 10.
+	Integer numberOfLegsWithActivityLogsLowSeverityLevel;
+	Integer numberOfLegsWithActivityLogsMediumSeverityLevel;
+	Integer numberOfLegsWithActivityLogsHighSeverityLevel;
 
-	// The crew members who were assigned with him or her in their last leg. 
-	Integer						crewMembersAssignedInLastLeg;
+	// The crew members who were assigned with him or her in their last leg.
+	Integer crewMembersAssignedInLastLeg;
 
-	// Their flight assignments grouped by their statuses. 
-	HashMap<String, Integer>	numberOfFlightAssignmentsGroupedByTheirStatus;
+	// Their flight assignments grouped by their statuses.
+	Map<String, Integer> numberOfFlightAssignmentsGroupedByTheirStatus;
 
-	// The average, minimum, maximum, and standard deviation of the number of flight assignments they had in the last month.. 
+	// The average, minimum, maximum, and standard deviation of the number of flight
+	// assignments they had in the last month..
 
-	StatsFlightCrewMember		numberOfFlightAssignmentsInLastMonth	= new StatsFlightCrewMember();
+	StatsFlightCrewMember numberOfFlightAssignmentsInLastMonth = new StatsFlightCrewMember();
 
 	// Derived attributes -----------------------------------------------------
 
