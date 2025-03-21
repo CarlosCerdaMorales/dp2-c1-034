3eb0f8f800d4fb9ceb6bb90dd0f1587139e4cbad
Carlos Cerdá Morales <carcermor@alum.us.es>
2025-03-21 14:42:25 +0100

    fix: :bug: Fixing customer dashboard!

---
+++ src/main/java/acme/features/authenticated/customer/dashboard/CustomerDashboardRepository.java
@@ -0,0 +1,21 @@
+
+package acme.features.authenticated.customer.dashboard;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+
+@Repository
+public interface CustomerDashboardRepository extends AbstractRepository {
+
+	@Query("select l.airportArrival.city from Booking b join b.flight f join Leg l on l.flight.id = f.id where b.customer.id = :id order by l.scheduledDeparture ")
+	public List<String> getLastFiveDestinations(int id);
+
+	@Query("select sum(b.price.amount) from Booking b where b.customer.id = :id and b.purchaseMoment >= :delta")
+	public Double getMoneySpentInBookingsLastYear(int id, Date delta);
+
+}
