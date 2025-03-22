e5a57f696944d8a62f1297020977e034c3e05713
Carlos Cerdá Morales <carcermor@alum.us.es>
2025-03-22 14:34:41 +0100

    fix: :bug: D03 Customer dashboard done!

--- src/main/java/acme/features/authenticated/customer/dashboard/CustomerDashboardRepository.java
+++ src/main/java/acme/features/authenticated/customer/dashboard/CustomerDashboardRepository.java
@@ -18,4 +18,13 @@ public interface CustomerDashboardRepository extends AbstractRepository {
 	@Query("select sum(b.price.amount) from Booking b where b.customer.id = :id and b.purchaseMoment >= :delta")
 	public Double getMoneySpentInBookingsLastYear(int id, Date delta);
 
+	@Query("select b.travelClass, count(b) from Booking b where b.customer.id = :id group by b.travelClass")
+	public List<Object[]> getBookingsGroupedByTravelClass(int id);
+
+	@Query("select avg(b.price.amount), min(b.price.amount), max(b.price.amount), stddev(b.price.amount) from Booking b where b.customer.id = :id and b.purchaseMoment >= :delta")
+	public List<Object[]> getCostsOfBookingsLastFiveYears(int id, Date delta);
+
+	@Query("select booking.id, count(distinct i.passenger.id) as p from IsFrom i where i.booking.id in (select b.id from Booking b where b.customer.id = :id) group by i.booking.id")
+	public List<Object[]> getNumberOfPassengersInBookings(int id);
+
 }
