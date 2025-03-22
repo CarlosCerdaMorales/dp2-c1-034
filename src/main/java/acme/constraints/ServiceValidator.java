3114d7207df1059062024b4706f17f4543dad230
JoseLu2121 <josmorver@alum.us.es>
2025-03-22 20:01:10 +0100

    feat: :arrow_up: Service validator added

--- src/main/java/acme/constraints/ServiceValidator.java
+++ src/main/java/acme/constraints/ServiceValidator.java
@@ -3,13 +3,20 @@ package acme.constraints;
 
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import acme.client.components.validation.AbstractValidator;
 import acme.client.components.validation.Validator;
 import acme.client.helpers.MomentHelper;
 import acme.entities.service.Service;
+import acme.entities.service.ServiceRepository;
 
 @Validator
 public class ServiceValidator extends AbstractValidator<ValidService, Service> {
+
+	@Autowired
+	private ServiceRepository repository;
+
 
 	@Override
 	protected void initialise(final ValidService annotation) {
@@ -28,6 +35,11 @@ public class ServiceValidator extends AbstractValidator<ValidService, Service> {
 			String promotionCode = service.getPromotionCode();
 			String codeYear = promotionCode.substring(promotionCode.length() - 2);
 			String actualYear = MomentHelper.getCurrentMoment().toString().substring(MomentHelper.getCurrentMoment().toString().length() - 2);
+			boolean uniqueService;
+			Service existingservice = this.repository.findServiceByPromoCode(service.getPromotionCode());
+			uniqueService = existingservice == null || existingservice.equals(service);
+
+			super.state(context, uniqueService, "promotionalCode", "acme.validation.service.duplicated-promocode.message");
 			{
 				if (!codeYear.equals(actualYear))
 					super.state(context, false, "promotionCode", "acme.validation.service.invalid-promotionCode-year.message");
