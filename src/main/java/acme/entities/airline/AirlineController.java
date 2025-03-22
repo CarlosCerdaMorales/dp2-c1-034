36f2dceb3aba5325cdbccde8b68a86d15d632a61
SergioCantilloBlanco <14sergiocb@gmail.com>
2025-03-22 16:03:49 +0100

    feat: :sparkles: Controller, Repository and Service initial creation

---
+++ src/main/java/acme/entities/airline/AirlineController.java
@@ -0,0 +1,11 @@
+
+package acme.entities.airline;
+
+import acme.client.components.principals.Authenticated;
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+
+@GuiController
+public class AirlineController extends AbstractGuiController<Authenticated, Airline> {
+
+}
