79b0c22dcc80722f40475a05484bf37a4df9c693
SergioCantilloBlanco <14sergiocb@gmail.com>
2025-03-22 19:28:32 +0100

    feat: :sparkles: Airline validator correction

---
+++ src/main/java/acme/features/entities/airline/AirlineRepository.java
@@ -0,0 +1,16 @@
+
+package acme.features.entities.airline;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.airline.Airline;
+
+@Repository
+public interface AirlineRepository extends AbstractRepository {
+
+	@Query("select a from Airline a where a.iata = :iata")
+	public Airline findAirlineByIATACode(String iata);
+
+}
