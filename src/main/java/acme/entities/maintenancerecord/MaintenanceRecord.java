575b3a79b5cf33bd513d10eef6e3041bc8cb82b1
ClaudiaOviedoGovantes <claudiaogvizcaino@gmail.com>
2025-03-22 18:28:10 +0100

    fix:

--- src/main/java/acme/entities/maintenancerecord/MaintenanceRecord.java
+++ src/main/java/acme/entities/maintenancerecord/MaintenanceRecord.java
@@ -17,6 +17,7 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
+import acme.constraints.ValidMaintenanceRecord;
 import acme.entities.aircraft.Aircraft;
 import acme.realms.Technician;
 import lombok.Getter;
@@ -25,6 +26,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@ValidMaintenanceRecord
 public class MaintenanceRecord extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
