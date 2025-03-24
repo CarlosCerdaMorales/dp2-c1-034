89257e28fb7271e3c1c781c4e80b4b5541906569
SergioCantilloBlanco <14sergiocb@gmail.com>
2025-03-24 20:15:18 +0100

    feat: :sparkles: Airline List Service added

---
+++ src/main/java/acme/features/entities/airline/AdministratorAirlineListService.java
@@ -0,0 +1,42 @@
+
+package acme.features.entities.airline;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.services.AbstractGuiService;
+import acme.entities.airline.Airline;
+
+public class AdministratorAirlineListService extends AbstractGuiService<Administrator, Airline> {
+
+	@Autowired
+	private AirlineRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		List<Airline> airlines;
+
+		airlines = this.repository.findAllAirlines();
+
+		super.getBuffer().addData(airlines);
+	}
+
+	@Override
+	public void unbind(final Airline airline) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(airline, "name", "iata", "website", "type", "foundationMoment", "email", "phoneNumber");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
