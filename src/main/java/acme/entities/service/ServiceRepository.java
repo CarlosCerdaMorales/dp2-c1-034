70143a82979dc1bf7433b1c35fba287bc204f99c
JoseLu2121 <josmorver@alum.us.es>
2025-03-25 09:24:50 +0100

    feat: :egg: D03 R14 group added

--- src/main/java/acme/entities/service/ServiceRepository.java
+++ src/main/java/acme/entities/service/ServiceRepository.java
@@ -1,6 +1,8 @@
 
 package acme.entities.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 
 import acme.client.repositories.AbstractRepository;
@@ -9,4 +11,8 @@ public interface ServiceRepository extends AbstractRepository {
 
 	@Query("select s from Service s where s.promotionCode = :promoCode")
 	public Service findServiceByPromoCode(String promoCode);
+
+	@Query("select s from Service s order by fuction('RAND')")
+	public Page<Service> findRandomService(Pageable pageable);
+
 }
