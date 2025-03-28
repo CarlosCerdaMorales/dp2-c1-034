ac9854777f0f2963573984a065df71fe97055622
Carlos Cerdá Morales <carcermor@alum.us.es>
2025-03-28 22:44:55 +0100

    feat: :sparkles: DONE FINALLY S2 MANDATORY REQUIREMENTS

---
+++ src/main/java/acme/features/customer/isFrom/CustomerIsFromCreateService.java
@@ -0,0 +1,97 @@
+
+package acme.features.customer.isFrom;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.booking.Booking;
+import acme.entities.passenger.Passenger;
+import acme.realms.Customer;
+import acme.relationships.IsFrom;
+
+@GuiService
+public class CustomerIsFromCreateService extends AbstractGuiService<Customer, IsFrom> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private CustomerIsFromRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		IsFrom isFrom;
+
+		isFrom = new IsFrom();
+
+		super.getBuffer().addData(isFrom);
+	}
+
+	@Override
+	public void bind(final IsFrom isFrom) {
+		int bId;
+		int pId;
+		Booking booking;
+		Passenger passenger;
+
+		bId = super.getRequest().getData("booking", int.class);
+		pId = super.getRequest().getData("passenger", int.class);
+		booking = this.repository.findBookingFromId(bId);
+		passenger = this.repository.findPassengerFromId(pId);
+
+		super.bindObject(isFrom, "booking", "passenger");
+
+		isFrom.setBooking(booking);
+		isFrom.setPassenger(passenger);
+
+	}
+
+	@Override
+	public void validate(final IsFrom isFrom) {
+		//boolean confirmation;
+
+		//confirmation = super.getRequest().getData("confirmation", boolean.class);
+		//super.state(confirmation, "confirmation", "acme.validation.confirmation.message");
+	}
+
+	@Override
+	public void perform(final IsFrom isFrom) {
+		this.repository.save(isFrom);
+	}
+
+	@Override
+	public void unbind(final IsFrom isFrom) {
+		Collection<Booking> bookings;
+		Collection<Passenger> passengers;
+		SelectChoices books;
+		SelectChoices passs;
+		Dataset dataset;
+		int customerId = super.getRequest().getPrincipal().getActiveRealm().getId();
+
+		bookings = this.repository.findBookingsFromCustomerId(customerId);
+		passengers = this.repository.findPassengersFromCustomerId(customerId);
+		books = SelectChoices.from(bookings, "locatorCode", isFrom.getBooking());
+		passs = SelectChoices.from(passengers, "passport", isFrom.getPassenger());
+		dataset = super.unbindObject(isFrom, "booking", "passenger", "draftMode");
+		dataset.put("booking", books.getSelected().getKey());
+		dataset.put("bookings", books);
+		dataset.put("passenger", passs.getSelected().getKey());
+		dataset.put("passengers", passs);
+
+		super.getResponse().addData(dataset);
+
+	}
+
+}
---
+++ src/main/resources/WEB-INF/views/customer/is-from/list.jsp
@@ -0,0 +1,13 @@
+<%@page%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" uri="http://acme-framework.org/"%>
+
+<acme:list> 
+	<acme:list-column code="customer.booking.list.locatorCode" path="booking.locatorCode"/>
+	<acme:list-column code="customer.passenger.list.passport" path="passenger.passport"/>
+</acme:list>
+
+<jstl:if test="${_command == 'list'}">
+	<acme:button code="customer.is-from.list.button.create" action="/customer/is-from/create"/>
+</jstl:if>	
