e9d288097db2bac648f4057ade53debb5e5e9840
luna <loufouela@alum.us.es>
2025-03-31 20:06:26 +0200

    feat: :sparkles: Added activityLog controller

---
+++ src/main/java/acme/features/flightCrewMember/activityLog/FlightCrewMemberActivityLogShowService.java
@@ -0,0 +1,12 @@
+
+package acme.features.flightCrewMember.activityLog;
+
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.activitylog.ActivityLog;
+import acme.realms.flightcrewmember.FlightCrewMember;
+
+@GuiService
+public class FlightCrewMemberActivityLogShowService extends AbstractGuiService<FlightCrewMember, ActivityLog> {
+
+}
