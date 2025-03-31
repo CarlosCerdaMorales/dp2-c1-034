19918bdcfb067ba2bc0288ad7941754f6f5737e1
Loubna Founoun elAoud <loufouela@alum.us.es>
2025-03-31 22:22:56 +0200

    feat: :sparkles: Added listing feature (todo handling null validation)

---
+++ src/main/java/acme/features/flightCrewMember/activityLog/FlightCrewMemberActivityLogRepository.java
@@ -0,0 +1,20 @@
+
+package acme.features.flightCrewMember.activityLog;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.activitylog.ActivityLog;
+import acme.entities.flightassignment.FlightAssignment;
+
+public interface FlightCrewMemberActivityLogRepository extends AbstractRepository {
+
+	@Query("select l from ActivityLog l where l.flightAssignment.id = :id")
+	List<ActivityLog> findLogsByFlightAssignment(int id);
+
+	@Query("select a from FlightAssignment a where a.id = :id ")
+	FlightAssignment findFlightAssignmentById(int id);
+
+}
